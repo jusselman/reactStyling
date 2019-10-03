f6048995e2deb94d06f2f5f6133d9d914ccd9f92
jusselman <usselmanjosh@gmail.com>
2019-10-02 21:11:22 -0700

    fix peg color

--- src/components/GuessPeg/GuessPeg.jsx
+++ src/components/GuessPeg/GuessPeg.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import styles from './GuessPeg.module.css';
 
 const GuessPeg = (props) => (
   <div>
-    {props.color}
+    <button
+      className={styles.button}
+      style={{
+        backgroundColor: props.color,
+        borderColor: props.color
+      }}
+    />
   </div>
+
 );
 
 export default GuessPeg;
